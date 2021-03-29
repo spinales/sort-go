faa2942db32540da003a7fd2967e3b98b7d77b3f
Saul Pinales <saul021002@gmail.com>
2021-03-29 12:06:49 -0400

    <feat>: flag -b agregada, ignorar espacios en blanco

--- main.go
+++ main.go
@@ -8,22 +8,41 @@ import (
 	"strings"
 )
 
+var b bool // -b, --ignore-leading-blanks  ignore leading blanks
+
 func main() {
+	// -b, --ignore-leading-blanks  ignore leading blanks
+	flag.BoolVar(&b, "b", false, "ignore leading blanks")
+	flag.BoolVar(&b, "ignore-leading-blanks", false, "ignore leading blanks")
 	flag.Parse()
 
+	// recibo todos los los parametros
 	for _, f := range flag.Args() {
-		sorting(f)
+		sorting(f, b)
 	}
 }
 
-func sorting(filename string) {
+// funcionamiento de variables por orden:
+// filename: nombre del archivo
+// ilb: ignorar espacios en blanco
+func sorting(filename string, ilb bool) {
 	data := openFile(filename)
-	arr := strings.Split(string(data), "\n")
-	sort.Strings(arr)
-	result := strings.Join(arr[:], "\n")
-	fmt.Println(result)
+	if ilb {
+		// elimino espacios en blanco
+		spaces := strings.ReplaceAll(string(data), "\n\n", "\n")
+		arr := strings.Split(spaces, "\n")
+		sort.Strings(arr)
+		fmt.Println(strings.Join(arr[:], "\n"))
+	} else {
+		// divido el archivo por cada linea
+		// organizo, e imprimo
+		arr := strings.Split(string(data), "\n")
+		sort.Strings(arr)
+		fmt.Println(strings.Join(arr[:], "\n"))
+	}
 }
 
+// abre el archivo por la ruta pasada
 func openFile(filename string) []byte {
 	dat, err := ioutil.ReadFile(filename)
 	if err != nil {
