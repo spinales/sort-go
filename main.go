345288e0ffa0f42a25e7aabe6a6163996fb8840b
Saul Pinales <saul021002@gmail.com>
2021-03-30 19:23:24 -0400

    <feat>: flag -d agregada

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 	"strings"
 )
 
-var b bool // -b, --ignore-leading-blanks  ignore leading blanks
+var (
+	b bool // -b, --ignore-leading-blanks
+	d bool // -d, --dictionary-order
+)
 
 func main() {
+	// -d, --dictionary-order      consider only blanks and alphanumeric characters
+	flag.BoolVar(&d, "d", false, "consider only blanks and alphanumeric characters")
+	flag.BoolVar(&d, "dictionary-order", false, "consider only blanks and alphanumeric characters")
 	// -b, --ignore-leading-blanks  ignore leading blanks
 	flag.BoolVar(&b, "b", false, "ignore leading blanks")
 	flag.BoolVar(&b, "ignore-leading-blanks", false, "ignore leading blanks")
@@ -18,28 +24,33 @@ func main() {
 
 	// recibo todos los los parametros
 	for _, f := range flag.Args() {
-		sorting(f, b)
+		sorting(f, b, d)
 	}
 }
 
 // funcionamiento de variables por orden:
 // filename: nombre del archivo
 // ilb: ignorar espacios en blanco
-func sorting(filename string, ilb bool) {
+// dict: ordenar por diccionario
+func sorting(filename string, ilb bool, dict bool) {
 	data := openFile(filename)
 	if ilb {
 		// elimino espacios en blanco
 		spaces := strings.ReplaceAll(string(data), "\n\n", "\n")
-		arr := strings.Split(spaces, "\n")
-		sort.Strings(arr)
-		fmt.Println(strings.Join(arr[:], "\n"))
+		printDefault(spaces)
+	} else if dict {
+		printDefault(string(data))
 	} else {
-		// divido el archivo por cada linea
-		// organizo, e imprimo
-		arr := strings.Split(string(data), "\n")
-		sort.Strings(arr)
-		fmt.Println(strings.Join(arr[:], "\n"))
+		printDefault(string(data))
 	}
+}
+
+func printDefault(data string) {
+	// divido el archivo por cada linea
+	// organizo, e imprimo
+	arr := strings.Split(data, "\n")
+	sort.Strings(arr)
+	fmt.Println(strings.Join(arr[:], "\n"))
 }
 
 // abre el archivo por la ruta pasada
