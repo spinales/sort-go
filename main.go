7d73273855a851cc0ec472877e66280a5203d0eb
Saul Pinales <saul021002@gmail.com>
2021-04-01 21:06:58 -0400

    <feat>: flag -C funcionamiento basico

--- main.go
+++ main.go
@@ -9,32 +9,47 @@ import (
 	"strings"
 )
 
-var c bool
+var (
+	c  bool // -c
+	cs bool // -C
+)
 
 func main() {
+	// -c
 	flag.BoolVar(&c, "c", false, `Check	that the single input file is ordered as specified by the arguments and the collating sequence of the current locale. 
 	Output shall not be sent to standard output. The exit code shall indicate whether or not disorder was detected or an error occurred.
 	If disorder  (or,  with -u, a duplicate key) is detected, a warning message shall be sent to standard error indicating where the
 	disorder or duplicate key was found.`)
+	// -C
+	flag.BoolVar(&cs, "C", false, `Same as -c, except that a warning message shall not be sent to standard error if  disorder  or,  with	-u,  a	duplicate  key	is
+	detected.`)
 	flag.Parse()
 
 	// recibo todos los los parametros
 	for _, file := range flag.Args() {
-		sorting(file, c)
+		if c || cs {
+		}
+		sorting(file)
 	}
 }
 
 // funcionamiento de variables por orden:
 // filename: nombre del archivo
-// sorted: para determinar si el archivo esta ordenado
-func sorting(filename string, sorted bool) {
+func sorting(filename string) {
 	data := openFile(filename)
-	if sorted {
+	if c {
 		arr := strings.Split(string(data), "\n")
 		if sort.StringsAreSorted(arr) {
 			os.Exit(0)
 		}
-		fmt.Fprintln(os.Stderr, "el archivo no esta ordenado")
+		fmt.Fprintln(os.Stderr, "El archivo no esta ordenado")
+		os.Exit(1)
+	} else if cs {
+		arr := strings.Split(string(data), "\n")
+		if sort.StringsAreSorted(arr) {
+			os.Exit(0)
+		}
+		fmt.Println("El archivo no esta ordenado")
 		os.Exit(1)
 	}
 	printDefault(string(data))
